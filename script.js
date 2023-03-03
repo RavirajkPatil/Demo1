40fb5a25108d59b006c86770e83f5120afeaf16a
RavirajkPatil <ravirajkpatil06@gmail.com>
2023-03-03 13:10:16 +0530

    changes in js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //document.addEventListener("click", function(){ alert("Hello World!"); });
-//document.querySelector("h1").addEventListener("click", function(){ alert("Just clicked"); });
-$("h1").click("click",function(){
-    alert("You just clicked the header");
-});
+document.querySelector("h1").addEventListener("click", function(){ alert("Just clicked the header"); });
+
+
+// $("h1").click("click",function(){
+//     alert("You just clicked the header");
+// });
