76189c3fcb59e759df1af6c67c51ce7bfdbb9087
RavirajkPatil <ravirajkpatil06@gmail.com>
2023-03-03 12:43:34 +0530

    script file added

---
+++ script.js
@@ -0,0 +1,5 @@
+//document.addEventListener("click", function(){ alert("Hello World!"); });
+//document.querySelector("h1").addEventListener("click", function(){ alert("Just clicked"); });
+$("h1").click("click",function(){
+    alert("You just clicked the header");
+});
